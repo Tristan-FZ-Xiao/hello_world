1561ef171bf3a669f5ea1b1a4d827343eda0686f
Tristan <tristan_xiao@qq.com>
2018-04-20 11:39:45 +0800

    add search method for multinode-tree

--- usr_tree.js
+++ usr_tree.js
@@ -15,7 +15,25 @@ function create_tree(root, max) {
         create_tree(root.subnode[i], max - 1)
 }
 
+function search_node(root, openId) {
+    var ret = null
+    if (root.subnode == null)
+        return null
+    for (var i = 0; i < root.subnode.length; i++) {
+        if (root.subnode[i].openId == openId)
+            return root.subnode[i]
+        else {
+            ret = search_node(root.subnode[i], openId)
+            if (ret != null) {
+                return ret
+            }
+        }
+    }
+    return null
+}
+
 var root = {}
 root.openId = "xxxx0"
-create_tree(root, 1)
-console.log(root)
+create_tree(root, 2)
+node = search_node(root, "xxxx2")
+console.log(node)
